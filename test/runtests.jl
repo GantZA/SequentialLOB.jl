f74169d7fb8b05607ae006995294c6c39f6a6290
Michael <michael.j.g.za@gmail.com>
2019-12-27 12:52:32 +0200

    added tests

---
+++ test/runtests.jl
@@ -0,0 +1,44 @@
+using SequentialLOB
+using Test
+
+include("../src/main.jl")
+
+@testset "All Tests" begin
+    slob_model = SLOB()
+    @testset "Reproducibility" begin
+        slob_model_1 = SLOB(1, 200, 238.745, 500, 2.0, 100,
+            2.0, 0.01, 0.01, SourceTerm(1.0, 0.5))
+        slob_model_2 = SLOB(1, 200, 238.745, 500, 2.0, 100,
+            2.0, 0.01, 0.01, SourceTerm(1.0, 0.5))
+        @test all(slob_model_1(45) .== slob_model_2(45))
+
+        @test all(slob_model_1(51) .== slob_model_2(51))
+
+        @test all(slob_model_1(4565756745) .== slob_model_2(4565756745))
+    end;
+
+    @testset "Default Values" begin
+        lob_densities, price_paths, mid_price_bars, P⁺s, P⁻s = slob_model()
+        @test size(mid_price_bars) == (101,1)
+    end;
+
+    @testset "Command Line Parse Default Arguments" begin
+        @test parse_commandline() == Dict("μ" => 0.5,
+            "num_paths" => 1,
+            "T" => 100,
+            "p₀" => 100.0,
+            "λ" => 1.0,
+            "SEED" => 1,
+            "β" => 1.0,
+            "nu" => 0.0,
+            "σ" => 1.0,
+            "D" => 4.0,
+            "M" => 100,
+            "L" => 50.0,
+            "α" => )
+    end
+
+    @testset "Main Default Arguments" begin
+        @test all(main("return") .== slob_model(1))
+    end
+end
