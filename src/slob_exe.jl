e602b1d6cc001a75f10e23b9eb6e1d54b86edb52
Michael <michael.j.g.za@gmail.com>
2019-12-27 12:52:59 +0200

    refactored compiled functions to use new names

--- src/slob_exe.jl
+++ src/slob_exe.jl
@@ -1,11 +1,11 @@
-module LOBMExec
+module SLOBExec
 
-using LatentOrderBookModel
+using SequentialLOB
 
 Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
     parsed_args = parse_commandline()
-    rdpp = ReactionDiffusionPricePaths(
-        parsed_args["num_paths"],
+    slob_model = SLOB(
+        1,
         parsed_args["T"],
         parsed_args["p₀"],
         parsed_args["M"],
@@ -22,8 +22,9 @@ Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
     price_paths,
     mid_price_bars,
     P⁺s,
-    P⁻s = rdpp(parsed_args["SEED"])
-    print(price_paths[:, 1])
+    P⁻s,
+    Ps = slob_model(parsed_args["SEED"])
+    print(mid_price_bars[:, 1])
     return 0
 end
 
